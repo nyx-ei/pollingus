5768ac1ebc1e4b2d380e5e3472c1717f692a9443
Alexis C. TADIFO <atadifo@emergensys.net>
2022-12-09 08:47:48 -0500

    Creation of the java project with Spring Initializer

---
+++ app4java/src/main/java/com/nyxei/pollingus/PollingusApplication.java
@@ -0,0 +1,13 @@
+package com.nyxei.pollingus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingusApplication.class, args);
+	}
+
+}
