396b4b3fd30f8b46f2276cff8288711728cc28a3
Pattykev <trishasoftwareengineer@gmail.com>
2023-02-08 13:38:05 +0100

    structuration

--- src/main/java/com/nyxei/pollingus/PollingusApplication.java
+++ src/main/java/com/nyxei/pollingus/PollingusApplication.java
@@ -1,26 +1,13 @@
 package com.nyxei.pollingus;
 
-import com.nyxei.pollingus.controllers.Digester;
-import org.commonmark.node.Node;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class PollingusApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(PollingusApplication.class, args);
-		//f
-		Digester.lectureMd("markdown-syntax/yes-no-question.md");
-		Digester.lectureMd("markdown-syntax/affiliate-question.md");
-		Parser parser = Parser.builder().build();
-		Node document = parser.parse("This is *Sparta*");
-		HtmlRenderer renderer = HtmlRenderer.builder().build();
-		renderer.render(document);
+		SpringApplication.run(PollingusApplication.class, args);
 	}
 
 }
-
