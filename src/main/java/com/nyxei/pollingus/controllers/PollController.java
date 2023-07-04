28d23103bfb3de712bf61397c59f935ab4578b29
lonla <gatienjordanlonlaep@gmail.com>
2023-07-04 09:58:33 +0100

    poll basic template trial

--- src/main/java/com/nyxei/pollingus/controllers/PollController.java
+++ src/main/java/com/nyxei/pollingus/controllers/PollController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 @Controller
 @RequestMapping("/polls")
--- src/main/java/com/nyxei/pollingus/models/Contact.java
+++ src/main/java/com/nyxei/pollingus/models/Contact.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Contact {
     @Id
