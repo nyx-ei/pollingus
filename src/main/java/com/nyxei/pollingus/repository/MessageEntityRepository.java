e0332251e5d5674a192cf369c5943cff830e1c98
Pattykev <trishasoftwareengineer@gmail.com>
2023-08-03 10:03:18 +0100

    fix: saving sender informations in webhookWhatsapp

---
+++ src/main/java/com/nyxei/pollingus/repository/MessageEntityRepository.java
@@ -0,0 +1,12 @@
+package com.nyxei.pollingus.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nyxei.pollingus.model.MessageEntity;
+
+
+@Repository
+public interface MessageEntityRepository extends CrudRepository<MessageEntity, Integer> {
+}
--- src/test/java/com/nyxei/pollingus/TestUtils.java
+++ src/test/java/com/nyxei/pollingus/TestUtils.java
@@ -3,7 +3,7 @@ package com.nyxei.pollingus;
 /*fill the variables with your data*/
 public class TestUtils {
 
-    public final static String TOKEN = "EAALhRzlRKlkBAJdoibHboNEcCZAwAMNOjCptXtkXUd0OV3KFAmd0ovEZBovE1ZCdYXyzD40Eg2zDGiUj8mHxsQZBeXdS7zUVEIGGDwyt8rQDa8zanXVBvQyZA9TFIDc1aJauxjEame60abha0gMDGyjmSSQMq5ZCM7W9bC4WFnkj7cKwbrnebrqp9QYuRZCpEX7waOrhxJA9iI1qJZA5TCNr";//"your_token";
+    public final static String TOKEN = "EAALhRzlRKlkBOZBNiLxBCvSMBombZA39aDCa7yKqRpFB8qAPRFrcBWwz8CJwJf2lMFOw2YRggAz8BHXPj3BdUZBay3mZCdZAepv6tDIRNKt4LpZAEDK262DtZAS8kQGba9s10USOAxzn7SkorDFEBXaGSxJliwjKg6ytOvdYcNODQjaHWxh46srS32IkV4DD4JVvQgrEewZC4VT6blgw4WZBR7LFQFZCVn";//"your_token";
     public final static String WABA_ID ="116998968082966";//"your_whatsapp_business_account";
     public final static String PHONE_NUMBER_1 = "237697887157";//"your_phone_number";
     public final static String PHONE_NUMBER_ID = "113799835071313";//"your_phone_numberID";
