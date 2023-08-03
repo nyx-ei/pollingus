b5bc505433240c6fd0bc7befdf1b97035f965c9c
TCHINGUÉ MAKAMTÉ Patricia Kévine <101264420+Pattykev@users.noreply.github.com>
2023-08-03 21:08:47 +0100

    Update TestUtils.java

--- src/test/java/com/nyxei/pollingus/TestUtils.java
+++ src/test/java/com/nyxei/pollingus/TestUtils.java
@@ -3,9 +3,9 @@ package com.nyxei.pollingus;
 /*fill the variables with your data*/
 public class TestUtils {
 
-    public final static String TOKEN = "EAALhRzlRKlkBOZBNiLxBCvSMBombZA39aDCa7yKqRpFB8qAPRFrcBWwz8CJwJf2lMFOw2YRggAz8BHXPj3BdUZBay3mZCdZAepv6tDIRNKt4LpZAEDK262DtZAS8kQGba9s10USOAxzn7SkorDFEBXaGSxJliwjKg6ytOvdYcNODQjaHWxh46srS32IkV4DD4JVvQgrEewZC4VT6blgw4WZBR7LFQFZCVn";//"your_token";
-    public final static String WABA_ID ="116998968082966";//"your_whatsapp_business_account";
-    public final static String PHONE_NUMBER_1 = "237697887157";//"your_phone_number";
-    public final static String PHONE_NUMBER_ID = "113799835071313";//"your_phone_numberID";
+    public final static String TOKEN = "your_token";
+    public final static String WABA_ID = "your_whatsapp_business_account";
+    public final static String PHONE_NUMBER_1 = "your_phone_number";
+    public final static String PHONE_NUMBER_ID = "your_phone_numberID";
 }
 
